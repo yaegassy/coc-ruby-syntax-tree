406c48147f800ffc6ff955a0b43b295502b9864d
yaegassy <yaegassy@users.noreply.github.com>
2022-11-08 12:50:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# coc-ruby-syntax-tree
+
+> fork from a [ruby-syntax-tree/vscode-syntax-tree](https://github.com/ruby-syntax-tree/vscode-syntax-tree)
+
+[coc.nvim](https://github.com/neoclide/coc.nvim) support for the [syntax_tree](https://github.com/ruby-syntax-tree/syntax_tree) gem
+
+## **!!Note & Warning!!**
+
+I don't use ruby regularly, so if you like ruby, please refer to this repository and create `coc-ruby-syntax-tree` by yourself.
+
+## Install
+
+You need to have [coc.nvim](https://github.com/neoclide/coc.nvim) installed for this extension to work.
+
+**e.g. vim-plug**:
+
+```vim
+Plug 'yaegassy/coc-ruby-syntax-tree', {'do': 'yarn install --frozen-lockfile'}
+```
+
+## Configuration options
+
+- `syntaxTree.enable`: Enable coc-ruby-syntax-tree extension, default: `true`
+- `syntaxTree.advanced.commandPath`: Absolute path to stree executable, default: `""`
+- `syntaxTree.additionalPlugins`: Registers [extra behaviors](https://github.com/ruby-syntax-tree/syntax_tree#plugins) with the language server, default: `[]`
+- `syntaxTree.ignoreFiles`: A glob pattern of files to ignore for formatting, default: `""`
+- `syntaxTree.printWidth`: The width to be used when formatting code, default: `null`
+- `syntaxTree.singleQuotes`: Uses single-quoted strings when possible, default: `false`
+- `syntaxTree.trailingComma`: Adds a trailing comma to multi-line array literals, hash literals, and method parameters, default: `false`
+
+## Commands
+
+- `syntaxTree.start`: Syntax Tree: Start
+- `syntaxTree.stop`: Syntax Tree: Stop
+- `syntaxTree.restart`: Syntax Tree: Restart
+- `syntaxTree.showOutputChannel`: Syntax Tree: Show Output Channel
+
+## Thanks
+
+- [ruby-syntax-tree/syntax_tree](https://github.com/ruby-syntax-tree/syntax_tree)
+- [ruby-syntax-tree/vscode-syntax-tree](https://github.com/ruby-syntax-tree/vscode-syntax-tree)
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,180 @@
+import { commands, Executable, ExtensionContext, LanguageClient, Uri, window, workspace } from 'coc.nvim';
+
+import { exec } from 'child_process';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import { promisify } from 'util';
+
+const promiseExec = promisify(exec);
+
+let languageClient: LanguageClient | null = null;
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const outputChannel = window.createOutputChannel('Syntax Tree');
+
+  context.subscriptions.push(
+    outputChannel,
+    commands.registerCommand('syntaxTree.start', startLanguageServer),
+    commands.registerCommand('syntaxTree.stop', stopLanguageServer),
+    commands.registerCommand('syntaxTree.restart', restartLanguageServer),
+    commands.registerCommand('syntaxTree.showOutputChannel', () => outputChannel.show()),
+    workspace.onDidChangeConfiguration((event) => {
+      if (event.affectsConfiguration('syntaxTree')) {
+        restartLanguageServer();
+      }
+    })
+  );
+
+  await startLanguageServer();
+
+  function getCWD() {
+    if (workspace.workspaceFolders[0].uri) {
+      return Uri.parse(workspace.workspaceFolders[0].uri).fsPath;
+    } else {
+      return process.cwd();
+    }
+  }
+
+  async function getServerOptions(args: string[]): Promise<Executable> {
+    const advancedConfig = workspace.getConfiguration('syntaxTree.advanced');
+    let value = advancedConfig.get<string>('commandPath');
+
+    if (value) {
+      const substitution = new RegExp('\\$\\{([^}]*)\\}');
+
+      for (let match = substitution.exec(value); match; match = substitution.exec(value)) {
+        switch (match[1]) {
+          case 'cwd':
+            value = value.replace(match[0], process.cwd());
+            break;
+          case 'pathSeparator':
+            value = value.replace(match[0], path.sep);
+            break;
+          case 'userHome':
+            value = value.replace(match[0], os.homedir());
+            break;
+        }
+      }
+
+      try {
+        if (fs.statSync(value).isFile()) {
+          return { command: value, args };
+        }
+      } catch {
+        outputChannel.appendLine(`Ignoring bogus commandPath (${value} does not exist).`);
+      }
+    }
+
+    try {
+      const cwd = getCWD();
+      await promiseExec('bundle show syntax_tree', { cwd });
+      return { command: 'bundle', args: ['exec', 'stree'].concat(args), options: { cwd } };
+    } catch {
+      // Do nothing.
+    }
+
+    const executablePaths = await Promise.all(
+      (process.env.PATH || '')
+        .split(path.delimiter)
+        .filter((directory) => directory.includes('ruby'))
+        .map((directory) => {
+          const executablePath = path.join(directory, 'stree');
+
+          return fs.promises.stat(executablePath).then(
+            (stat) => (stat.isFile() ? executablePath : null),
+            () => null
+          );
+        })
+    );
+
+    for (const executablePath of executablePaths) {
+      if (executablePath) {
+        return { command: executablePath, args };
+      }
+    }
+
+    // Otherwise, fall back to the global stree lookup.
+    return { command: 'stree', args };
+  }
+
+  async function startLanguageServer() {
+    if (languageClient) {
+      return; // preserve idempotency
+    }
+
+    const config = workspace.getConfiguration('syntaxTree');
+
+    const args = ['lsp'];
+    const plugins = new Set<string>();
+
+    if (config.get<boolean>('singleQuotes')) {
+      plugins.add('plugin/single_quotes');
+    }
+
+    if (config.get<boolean>('trailingComma')) {
+      plugins.add('plugin/trailing_comma');
+    }
+
+    const additionalPlugins = config.get<string[]>('additionalPlugins');
+    if (additionalPlugins) {
+      additionalPlugins.forEach((plugin) => plugins.add(plugin));
+    }
+
+    if (plugins.size > 0) {
+      args.push(`--plugins=${Array.from(plugins).join(',')}`);
+    }
+
+    const printWidth = config.get<number>('printWidth');
+    if (printWidth) {
+      args.push(`--print-width=${printWidth}`);
+    }
+
+    const ignoreFiles = config.get<string>('ignoreFiles');
+    if (ignoreFiles) {
+      args.push(`--ignore-files=${ignoreFiles}`);
+    }
+
+    const run = await getServerOptions(args);
+    outputChannel.appendLine(`Starting language server: ${run.command} ${run.args?.join(' ')}`);
+
+    languageClient = new LanguageClient(
+      'Syntax Tree',
+      { run, debug: run },
+      {
+        documentSelector: [
+          { scheme: 'file', language: 'haml' },
+          { scheme: 'file', language: 'ruby' },
+          { scheme: 'file', pattern: '**/Gemfile' },
+        ],
+        outputChannel,
+      }
+    );
+
+    try {
+      await languageClient.start();
+    } catch (error) {
+      languageClient = null;
+      window.showErrorMessage(`stree (syntax_tree) not found.`);
+    }
+  }
+
+  async function stopLanguageServer() {
+    if (languageClient) {
+      outputChannel.appendLine('Stopping language server...');
+      await languageClient.stop();
+      languageClient = null;
+    }
+  }
+
+  async function restartLanguageServer() {
+    outputChannel.appendLine('Restarting language server...');
+    await stopLanguageServer();
+    await startLanguageServer();
+  }
+}
+
+export async function deactivate() {
+  await languageClient?.stop();
+  languageClient = null;
+}
